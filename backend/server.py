a7738f882761b4b3b9681de031e650a43632866c
Yan Wang <wang.yan8@northeastern.edu>
2025-12-06 15:25:12 -0800

    Add URL to CORS configuration

--- backend/server.py
+++ backend/server.py
@@ -19,7 +19,8 @@ CORS(app, resources={r"/*": {"origins": [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+    "https://adaptive-sql-learning.vercel.app"
 ]}})
 
 
